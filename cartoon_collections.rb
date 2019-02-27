787e992bd1c5ad49b0e86a3a1ad31be8b2db73ca
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-27 21:10:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,6 @@ end
 
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.collect {|cheese| #{list.include?(#{cheese)}} 
-  list.find {|a| a == #{cheese"}
-  else 
-  nil 
-end 
+  cheese_types.collect {|cheese| #{list.include?(#{cheese)}} 
 end
 
